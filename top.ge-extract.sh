b7f53977c5c70cc15da4cdd0f874a1ee38ca4cf7
Nick Chkhikvishvili <frrrredo@gmail.com>
2019-07-02 18:07:34 +0400

    Update top.ge-extract.sh

--- top.ge-extract.sh
+++ top.ge-extract.sh
@@ -7,6 +7,14 @@
 # license: BSD
 
 
+#Extraxt domains, URLs from top.ge by category. eg:
+
+# ./top.ge-extract.sh -c 23 
+# ./top.ge-extract.sh -c "http://www.top.ge/cat.php?c=19&where=Games"
+# ./top.ge-extract.sh -c 30 -d
+
+
+
 # Progress bar function                      
 function progress_start {
 

2f5fb0573d3e28e33b0c34b9a98d063ccad6bf8b
Nick Chkhikvishvili <frrrredo@gmail.com>
2021-04-20 21:39:00 +0400

    Update geo_names.sh

--- geo_names.sh
+++ geo_names.sh
@@ -13,6 +13,7 @@ export LC_ALL=en_US.UTF-8
 # how many names
 for name in {1..1000}; do
 
-encoded_name=$(curl -s "http://kids.ge/baby-name?id=$name" |grep  "<meta property=\"og:title\" content=" | grep -Po content.* | cut -d \- -f1 | cut -d \" -f2 |  perl -X -MURI::Escape -ne 'chomp;print uri_escape($_),"\n"')
+encoded_name=$(curl -s "http://kids.ge/baby-name?id=$name" |grep -Po "(?<=\<title\>).*(?=\ \-)" | awk '{print $1}')
 
 curl -s 'https://fonts.ge/ka/converter/' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: https://fonts.ge' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0' --data "convert=convert&in=utf8&out=lat1&text=$encoded_name" --compressed | grep "<td class=box4row1>" | grep -Po "nDblClick\=this\.select\(\)\>.*" |sed -e "s/\/textarea.*//g" |cut -d \< -f1 | cut -d \> -f2; done
+
